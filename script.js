4a93d9839fea04496859b10f9bda18dc16389eac
Amr <amr.atyia.ham@gmail.com>
2020-12-23 21:18:24 +0900

    remove base table and create confusion table

--- script.js
+++ script.js
@@ -1,248 +1,137 @@
-// // fill base table
-// function fillTable(accuracy){
-    
-//     var k = '<tbody>'
-//     for (let i=0; i < accuracy.length; i++){
-//         var acc = accuracy[i]
-//         if (i==0){
-//             k += '<tr>';
-//             k += '<th scope="row" rowspan="7" class="rotated">Subject</th>'
-//             k += '<th scope="row">2</th>'
-//             k += '<td>' + acc[0].toFixed(2)+ '</td>';
-//             k += '<td>' + acc[1].toFixed(2) + '</td>';
-//             k += '<td>' + acc[2].toFixed(2) + '</td>';
-//             k += '<td>' + acc[3].toFixed(2) + '</td>';
-//             k += '<td>' + acc[4].toFixed(2) + '</td>';
-//             k += '<td>' + acc[5].toFixed(2) + '</td>';
-//             k += '<td>' + acc[6].toFixed(2) + '</td>';
-//             k += '<td>' + acc[7].toFixed(2) + '</td>';
-//             k += '<td>' + acc[8].toFixed(2) + '</td>';
-//             k += '<td>' + acc[9].toFixed(2) + '</td>';
-//             k += '<tr>';
-//         } else if (i==1) {
-//             k += '<tr>';
-//             k += '<th scope="row">3</th>'
-//             k += '<td>' + acc[0].toFixed(2) + '</td>';
-//             k += '<td>' + acc[1].toFixed(2) + '</td>';
-//             k += '<td>' + acc[2].toFixed(2) + '</td>';
-//             k += '<td>' + acc[3].toFixed(2) + '</td>';
-//             k += '<td>' + acc[4].toFixed(2) + '</td>';
-//             k += '<td>' + acc[5].toFixed(2) + '</td>';
-//             k += '<td>' + acc[6].toFixed(2) + '</td>';
-//             k += '<td>' + acc[7].toFixed(2) + '</td>';
-//             k += '<td>' + acc[8].toFixed(2) + '</td>';
-//             k += '<td>' + acc[9].toFixed(2) + '</td>';
-//             k += '<tr>';
-//         } else if (i==2) {
-//             k += '<tr>';
-//             k += '<th scope="row">4</th>'
-//             k += '<td>' + acc[0].toFixed(2) + '</td>';
-//             k += '<td>' + acc[1].toFixed(2) + '</td>';
-//             k += '<td>' + acc[2].toFixed(2) + '</td>';
-//             k += '<td>' + acc[3].toFixed(2) + '</td>';
-//             k += '<td>' + acc[4].toFixed(2) + '</td>';
-//             k += '<td>' + acc[5].toFixed(2) + '</td>';
-//             k += '<td>' + acc[6].toFixed(2) + '</td>';
-//             k += '<td>' + acc[7].toFixed(2) + '</td>';
-//             k += '<td>' + acc[8].toFixed(2) + '</td>';
-//             k += '<td>' + acc[9].toFixed(2) + '</td>';
-//             k += '<tr>';
-//         } else if (i==3) {
-//             k += '<tr>';
-//             k += '<th scope="row">4 Front</th>'
-//             k += '<td>' + acc[0].toFixed(2) + '</td>';
-//             k += '<td>' + acc[1].toFixed(2) + '</td>';
-//             k += '<td>' + acc[2].toFixed(2) + '</td>';
-//             k += '<td>' + acc[3].toFixed(2) + '</td>';
-//             k += '<td>' + acc[4].toFixed(2) + '</td>';
-//             k += '<td>' + acc[5].toFixed(2) + '</td>';
-//             k += '<td>' + acc[6].toFixed(2) + '</td>';
-//             k += '<td>' + acc[7].toFixed(2) + '</td>';
-//             k += '<td>' + acc[8].toFixed(2) + '</td>';
-//             k += '<td>' + acc[9].toFixed(2) + '</td>';
-//             k += '<tr>';
-//         }
-//     }
-//     k += '</tbody>';
-//     document.getElementById('tableData').innerHTML = k;
-
-// };
-
-// // fill main table
-// function getData(object) {
-//     // call the main table
-//     var mainTable = document.querySelector('#main-table');
-//     var tableRows = mainTable.rows
-//     // iterate for every row
-//     for (var i = 1; i <= tableRows.length -1; i ++){ //9
-//         // iterate in row cells
-//         for (var j = 3; j <= 9; j += 2){
-//             console.log(i, j)
-//             tableRows[i].childNodes[j].innerHTML = '' // remove current cell value
-//             fillTable(object[`${i}_${j}`])
-//             console.log(object[`${i}_${j}`])
-
-//             // call the base table.
-//             baseTable = document.querySelector('#base-table')
-            
-//             // clone the basic table
-//             clonedTable = baseTable.cloneNode(true)
-//             // redisplay the cloned talbe
-//             clonedTable.style.display = 'table'
-//             tableRows[i].childNodes[j].appendChild(clonedTable)
-    
-//         }
-//     }
-
-//     };
-    
-// // fetch data from dummy json
-// fetch('dummy.json')
-
-//   .then(response => response.json())
-//   .then(jsonResponse => getData(jsonResponse))
-
-// console.log(document.body);
-
-
 // fetch data from dummy json
-fetch('test.json')
+fetch('final.json')
 
   .then(response => response.json())
   .then(jsonResponse => getData(jsonResponse))
 
-//   function deepInvestigate(data) {
-//     var method =Object.keys(data)
-//     for (var i = 1; i < method.length ; i ++){ 
-//         console.log(method)
-//         var filling = Object.keys(data[method[i]])
-//         for (var j = 1; j < filling.length ; j ++){ 
-//             console.log(filling)
-//             subjects = Object.keys(data[method[i]][filling[j]])
-//             for (var k = 1; k < subjects.length ; k ++){ 
-//                 var category = Object.keys(data[method[i]][filling[j]][subjects[k]])
-//                 for (var l = 1; l < subjects.length ; l ++){
-//                     var measures = data[method[i]][filling[j]][subjects[k]][category[l]]
-//                     console.log(measures)
-//                  }
-                
-//                 }
-
-//             }
-//         }
-//     }
-
 // investigate in one case
 function investigate(data) {
-    var rows = data['lightGBM']['GAIN']
-    fillTable_from_json(rows)
+    var rows = data['LightGBM']['amin']
+    // fillBaseTable(rows)
     console.log(rows)
-    }
-
-
-function fillTable_from_json(rows){
-    subjects = Object.keys(rows)
-    var k = '<tbody>'
-    for (let i=0; i < subjects.length; i++){
-        var subj = subjects[i]
-
-        if (subj=='123_0'){
-            console.log(subj)
-            k += '<tr>';
-            k += '<th scope="row" rowspan="7" class="rotated">Subject</th>'
-            k += '<th scope="row">2</th>'
-            k += '<td>' + parseFloat(rows[subj]['JT_L']['accuracy']).toFixed(2)+ '</td>';
-            k += '<td>' + parseFloat(rows[subj]['JT_R']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SJT']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_FLEX']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<tr>';
-        } else if (subj=='023_1') {
-            console.log(subj)
-            k += '<tr>';
-            k += '<th scope="row">3</th>'
-            k += '<td>' + parseFloat(rows[subj]['JT_L']['accuracy']).toFixed(2)+ '</td>';
-            k += '<td>' + parseFloat(rows[subj]['JT_R']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SJT']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_FLEX']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<tr>';
-        } else if (subj=='013_2') {
-            console.log(subj)
-            k += '<tr>';
-            k += '<th scope="row">4</th>'
-            k += '<td>' + parseFloat(rows[subj]['JT_L']['accuracy']).toFixed(2)+ '</td>';
-            k += '<td>' + parseFloat(rows[subj]['JT_R']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SJT']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_FLEX']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<tr>';
-        } else if (subj=='012_3') {
-            console.log(subj)
-            k += '<tr>';
-            k += '<th scope="row">4 Front</th>'
-            k += '<td>' + parseFloat(rows[subj]['JT_L']['accuracy']).toFixed(2)+ '</td>';
-            k += '<td>' + parseFloat(rows[subj]['JT_R']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SJT']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_E']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_FLEX']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Kn']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_L_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<td>' + parseFloat(rows[subj]['SENIOR_R_Crouch']['accuracy']).toFixed(2) + '</td>';
-            k += '<tr>';
-        }
-    }
-    k += '</tbody>';
-    document.getElementById('tableData').innerHTML = k;
-
-};
-
+    // createAccurayTable(rows)
+    var mat = rows['012_3']['SENIOR_L_E']['confusion_matrix']
+    createConfusionTable(mat)
+}
 
 // fill main table with all possible casses(method, filling)
-const method_mapping = {1:'lightGBM', 2:'NN_unbalanced', 3:'NN_balanced', 4:'SVM_lin', 5:'SVM_nl'}
-const filling_mapping = {3: 'GAIN', 5:'fmin', 7:'amin', 9:'zero'}
+const method_mapping = {1:'LightGBM', 2:'NN_unbalanced', 3:'NN_balanced', 4:'SVM_lin', 5:'SVM_nl'}
+const filling_mapping = {1: 'gain', 2:'fmin', 3:'amin', 4:'zero'}
 
 function getData(json_data) {
     // call the main table
     var mainTable = document.querySelector('#main-table');
-    var tableRows = mainTable.rows
     // iterate for every row
-    for (var i = 1; i <= tableRows.length -1; i ++){ //9
-        // iterate in row cells(3, 5, 7, 9)
-        for (var j = 3; j <= 9; j += 2){
-            console.log(i, j)
-            tableRows[i].childNodes[j].innerHTML = '' // remove current cell value
+    for (var i = 1; i < mainTable.rows.length; i ++){ //9
+        // iterate in every row cell
+        for (var j = 1; j < mainTable.rows[i].cells.length; j++){
+            console.log(mainTable.rows[i].cells[j])
+
+            // get base table data from json data
             var rows = json_data[method_mapping[i]][filling_mapping[j]]
-            fillTable_from_json(rows)
-            console.log(rows)
-
-            // call the base table.
-            baseTable = document.querySelector('#base-table')
+            // fill base table
             
-            // clone the basic table
-            clonedTable = baseTable.cloneNode(true)
-            // redisplay the cloned talbe
-            clonedTable.style.display = 'table'
-            tableRows[i].childNodes[j].appendChild(clonedTable)
-    
-        }
-    }
-
+            console.log(rows)
+            createAccurayTable(mainTable.rows[i].cells[j],rows)   
+        };
     };
+
+};
+
+
+function createConfusionTable(parent, mat){
+    var tbl  = document.createElement('table');
+    tbl.style.width  = '300px';
+    tbl.style.border = '1px solid none';
+    var tr1 = tbl.insertRow(0);
+    console.log(mat)
+    mat_keys = Object.keys(mat)
+    for (let i=0; i < mat_keys.length; i++){
+        console.log(mat_keys[i])
+        // head row
+        if (i==0){
+            var th = tr1.insertCell();
+            th.appendChild(document.createTextNode(''))
+        }
+        var th = tr1.insertCell();
+        th.appendChild(document.createTextNode(mat_keys[i]))
+        th.style.border = '1px solid blue';
+        th.style.fontWeight= 'bold'
+
+        // main body 
+        var tr = tbl.insertRow();
+        var val_keys = Object.keys(mat[mat_keys[i]])
+        for (let j=0; j < val_keys.length; j++){
+            if (j==0){
+                var td = tr.insertCell();
+                td.appendChild(document.createTextNode(val_keys[j]));
+                td.style.border = '1px solid red';
+                td.style.fontWeight= 'bold'
+            }
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(mat[mat_keys[i]][val_keys[j]]));
+            td.style.border = '1px solid black';
+
+        };   
+    };
+    parent.appendChild(tbl);
+};
+
+
+function createAccurayTable(parent, rows) {
+    var tbl  = document.createElement('table');
+    const subjs = Object.keys(rows);
+    sub_mapping = {"123_0":'2', "023_1":'3', "013_2":'4', "012_3":'4 Front'};
+
+    var tr0 = tbl.insertRow();
+    var tr1 = tbl.insertRow();
+    for (let i=0; i < subjs.length; i++){
+        console.log(subjs[i]);
+        if (i==0 ){
+            var th = tr0.insertCell();
+            th.appendChild(document.createTextNode(''))
+            var th = tr0.insertCell();
+            th.appendChild(document.createTextNode(''))
+            var td0 = tr0.insertCell();
+            td0.appendChild(document.createTextNode('category'));
+            td0.style.border = '1px solid red';
+            td0.style.fontWeight= 'bold';
+            td0.setAttribute('colSpan', '10');
+            var th = tr1.insertCell();
+            th.appendChild(document.createTextNode(''));
+            var th = tr1.insertCell();
+            th.appendChild(document.createTextNode(''));
+        };
+        
+        var tr = tbl.insertRow();
+        var categ = Object.keys(rows[subjs[i]])
+
+        // main body 
+        for (let j=0; j < categ.length; j++){
+            if (i==0){
+            var th = tr1.insertCell();
+            th.appendChild(document.createTextNode(categ[j]))
+            th.style.border = '1px solid blue';
+            th.style.fontWeight= 'bold'
+            };
+            if (i==0 && j==0){
+                var td = tr.insertCell();
+                td.appendChild(document.createTextNode('subject'));
+                td.style.border = '1px solid red';
+                td.style.fontWeight= 'bold'
+                td.setAttribute('rowSpan', '4');
+            };
+            if (j==0){
+                var td = tr.insertCell();
+                td.appendChild(document.createTextNode(sub_mapping[subjs[i]]));
+                td.style.border = '1px solid blue';
+                td.style.fontWeight= 'bold'
+            };
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(parseFloat(rows[subjs[i]][categ[j]]['accuracy']).toFixed(2)));
+            createConfusionTable(td, rows[subjs[i]][categ[j]]['confusion_matrix']);
+            td.style.border = '1px solid black';
+        };
+    };
+    parent.appendChild(tbl);  
+};
